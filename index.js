147edeb4756226fe9f93ee49f51fd697bea4591d
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-06-26 13:21:46 +0600

    make some api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +11,68 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Profast server is cooking");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.or0q8ig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const parcelCollection = client
+      .db("Profast")
+      .collection("parcelCollection");
+
+    app.get("/parcels", async (req, res) => {
+      const email = req.query.email;
+      const query = email ? { created_by: email } : {};
+
+      const options = {
+        creation_date: -1,
+      };
+
+      const result = await parcelCollection.find(query, options).toArray();
+      res.send(result);
+    });
+
+    app.get("/parcels/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await parcelCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+
+      const result = await parcelCollection.insertOne(parcel);
+      res.send(result);
+    });
+
+    app.delete("/parcels/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await parcelCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Profast server running on port ${port}`);
